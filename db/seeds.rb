acb7d88e0426eb7e8fae86ca3361c40027300893
Htet Wai Yan Aung <codinglearningc137@gmail.com>
2025-06-18 15:44:05 +0630

    preparing before hosting website especially seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,36 +3,11 @@ require 'faker'
 # Clear existing data (optional, uncomment if needed)
 # MUsersTJobOffer.delete_all
 # TJobOffer.delete_all
-# MUser.delete_all
-# MCompany.delete_all
 # TSkill.delete_all
 # TLocation.delete_all
 # TCategory.delete_all
-
-puts "Creating 5 random users..."
-5.times do
-  MUser.create!(
-    name: Faker::Name.name,
-    email: Faker::Internet.unique.email,
-    password: 'password123',
-    phone: Faker::PhoneNumber.cell_phone,
-    about: "私の目標はﾌﾟﾛｸﾞﾗﾏｰになることです。大学でJava、Android,Tomcat Server、MySql、UML、C、C ++、OOP、JavaScript、HTML、XMLを勉強しました。2016年2月に卒業しました。大学から勉強した言語を使っていろいろなﾌﾟﾛｼﾞｪｸﾄを開発しています。" # Fixed Japanese description for all users
-  )
-end
-
-puts "Creating 5 random companies..."
-5.times do
-  company = MCompany.create!(
-    name: Faker::Company.name,
-    email: Faker::Internet.unique.email,
-    password: 'password123',
-    phone: Faker::PhoneNumber.cell_phone,
-    info: "ウェアラブルコンピューティングのデバイスおよびシステム開発、GUIデザイン、ソフトウェアフュージョン、センサネットワークシステム開発、AR／画像処理システム開発などを手がけている、ウェアラブルデバイスのトップカンパニーです。",
-    address: "78st30st, ChanAyeTharZan Township, Mandalay"
-  )
-  # Assign a random location from TLocation to the company
-  company.update!(location: TLocation.sample)
-end
+# MUser.delete_all
+# MCompany.delete_all
 
 puts "Creating 7 random skills..."
 7.times do
@@ -49,49 +24,82 @@ puts "Creating 7 random categories..."
   TCategory.create!(title: Faker::Job.field)
 end
 
-# Fetch all records
-users = MUser.all
-companies = MCompany.all
+# Fetch all records after creation
 skills = TSkill.all
 locations = TLocation.all
 categories = TCategory.all
 
-puts "Creating 10 random job offers..."
-50.times do
-  TJobOffer.create!(
-    title: categories.sample.title, # Use category as title
-    minsalary: [100000, 200000, 300000, 400000].sample,
-    maxsalary: [300000, 400000, 500000, 600000].sample,
-    location: locations.sample,
-    category: categories.sample,
-    description: Faker::Lorem.paragraph_by_chars(number: 200, supplemental: false),
-    company: companies.sample
-  )
-end
-
-# Fetch job offers after creation
-job_offers = TJobOffer.all
-
-puts "Assigning 3-7 random skills to each job offer..."
-job_offers.each do |job_offer|
-  job_offer.t_skills = skills.sample(rand(3..7)) # Randomly select between 3 to 7 skills
-  job_offer.save!
-end
-
-puts "Assigning a random category and location to each user..."
-users.each do |user|
-  user.update!(
-    category: categories.sample, 
-    location: locations.sample
-  )
-end
-
-puts "Each user is applying to 7-11 random jobs..."
-users.each do |user|
-  applied_jobs = job_offers.sample(rand(7..11)) # Select random jobs between 7 to 11 jobs
-  applied_jobs.each do |job|
-    MUsersTJobOffer.create!(m_user: user, t_job_offer: job)
+puts "Creating 5 random users..."
+while MUser.count < 5
+  password = 'password'
+  begin
+    MUser.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.unique.email,
+      password: password,
+      password_confirmation: password,
+      phone: Faker::PhoneNumber.cell_phone,
+      category: categories.sample,
+      location: locations.sample
+    )
+  rescue ActiveRecord::RecordInvalid => e
+    puts "Error creating user: #{e.message}"
   end
+end
+
+puts "Assigning 2 to 4 random skills to each user..."
+MUser.all.each do |user|
+  random_skills = skills.sample(rand(2..4))
+  user.t_skills << random_skills.reject { |s| user.t_skills.include?(s) }
+end
+
+puts "Creating 5 random companies..."
+while MCompany.count < 5
+  location = locations.sample
+  begin
+    MCompany.create!(
+      name: Faker::Company.name,
+      email: Faker::Internet.unique.email,
+      password: 'password',
+      password_confirmation: 'password',
+      phone: Faker::PhoneNumber.cell_phone,
+      location: location,
+      info: '私たちはテクノロジーソリューションとソフトウェア開発サービスの分野でリーディングカンパニーです。',
+      address: Faker::Address.full_address
+    )
+  rescue ActiveRecord::RecordInvalid => e
+    puts "Error creating company: #{e.message}"
+  end
+end
+
+puts "Creating job offers for each company..."
+MCompany.all.each do |company|
+  rand(2..4).times do
+    job_skills = skills.sample(rand(2..4))
+    job_location = locations.sample
+    job_category = categories.sample
+
+    begin
+      company.t_job_offers.create!(
+        title: Faker::Job.title,
+        description: Faker::Lorem.paragraph(sentence_count: 5),
+        location: job_location,
+        category: job_category,
+        t_skills: job_skills,
+        minsalary: rand(5000000..7000000).to_s,
+        maxsalary: rand(8000000..10000000).to_s
+      )
+    rescue ActiveRecord::RecordInvalid => e
+      puts "Error creating job offer for #{company.name}: #{e.message}"
+    end
+  end
+end
+
+puts "Making users apply to random job offers..."
+job_offers = TJobOffer.all
+MUser.all.each do |user|
+  offers_to_apply = job_offers.sample(rand(2..4))
+  user.t_job_offers << offers_to_apply.reject { |offer| user.t_job_offers.include?(offer) }
 end
 
 puts "Seeding completed successfully!"
